fbffd57a36d13ce4394cd13319321c0da18b3ec5
Riski_ <riskiafrizal11@gmail.com>
2026-06-18 11:55:53 +0700

    Import soft login styles

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import './playful-professional.css';
 import './navigation-fix.css';
 import './notifications.css';
 import './settings-menu.css';
+import './auth-soft-login.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
