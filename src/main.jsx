cd60d2d3649698cb0bf1461e921ca010148b5567
Riski_ <riskiafrizal11@gmail.com>
2026-06-18 11:25:41 +0700

    Import settings menu styles

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import './budget-allocation.css';
 import './playful-professional.css';
 import './navigation-fix.css';
 import './notifications.css';
+import './settings-menu.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
